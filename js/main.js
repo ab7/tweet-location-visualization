4c5847691f3bab344cd5fe0aaba8989cc3657c4d
Alex Baker <robotsmack@gmail.com>
2014-04-23 01:09:13 -0600

    now plots markers based on tweet location

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
+var openedInfoWindow = null;
+// make info window
+function makeInfoWin (marker, text, map) {
+  var infoWin = new google.maps.InfoWindow({
+    content: text
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    if (openedInfoWindow != null) openedInfoWindow.close();
+    infoWin.open(map, marker);
+    openedInfoWindow = infoWin;
+    google.maps.event.addListener(infoWin, 'closeclick', function() {
+      openedInfoWindow = null;
+    });
+  });
+}
+
 // make new map object and add markers
 function makeMap(coords) {
   var mapOptions, map, makeStyles, styledMap, myLatlng, marker;
   mapOptions = {
     center: new google.maps.LatLng(35, 0),
-    // draggable: false,
-    // disableDoubleClickZoom: true,
-    // disableDefaultUI: true,
-    // scrollwheel: false,
-    zoom: 2
+    zoom: 1
   };
   $.getJSON('/js/mapstyle.json', function (data) {
     mapStyles = data;
-    styledMap = new google.maps.StyledMapType(mapStyles, {name: "Styled Map"});
+    styledMap = new google.maps.StyledMapType(mapStyles, {name: 'Styled Map'});
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
     map.mapTypes.set('map_style', styledMap);
     map.setMapTypeId('map_style');
     
     // add markers to map
-    for (i = 0; i < coords.length; i++) {
-      myLatlng = new google.maps.LatLng(coords[i][0], coords[i][1]);
+    for (var i = 0; i < coords.length; i++) {
+      myLatlng = new google.maps.LatLng(coords[i][0][0], coords[i][0][1]);
       marker = new google.maps.Marker({
         position: myLatlng,
         map: map
       });
+      var tweet = coords[i][1][1].toString()
+      makeInfoWin(marker, tweet, map);
     }
   });
 }
@@ -39,13 +53,17 @@ function getCoords(geoname) {
 
 // find location of tweets
 function findLoc(tweet) {
-  var results, loc, coords, lng, lat, c;
-  coords = [];  // Here is my empty coordinates array
+  var results, loc, coords, lng, lat, locEnabled, noData, userLoc;
+  coords = [];
+  locEnabled = 0;
+  noData = 0;
+  userLoc = 0;
   result = $('.result');
   result.empty();
   $.each(tweet.statuses, function (i, item) {
     if (item.geo) {
-      coords.push(item.geo.coordinates); // The coords push just fine right here
+      coords.push([item.geo.coordinates, [item.geo.coordinates, item.text]]);
+      locEnabled++;
     } else if (item.user.location) {
       // call the geonames api for location data
       $.ajax({
@@ -58,15 +76,23 @@ function findLoc(tweet) {
           username: 'ab77'
         }
       })
-        .done(function (data) {
-          c = getCoords(data);
-          if (c) coords.push(c); // But they will not push to the same coords array here
+        .done(function(data) {
+          var c = getCoords(data);
+          if (c) {
+            coords.push([c, [item.user.location, item.text]]);
+            userLoc++;
+          }
+          if ((i + 1) === tweet.statuses.length) {
+            $('.noData').text(noData);
+            $('.noGeo').text(userLoc);
+            $('.locEnabled').text(locEnabled);
+            makeMap(coords);
+          }
         });
     } else {
-      console.log('nothin');
+      noData++;
     }
   });
-  makeMap(coords); // Here is where I pass the coordinates in
 }
 
 // send hashtag to twitter server and retrieve json
@@ -83,5 +109,6 @@ function getTweets(e) {
 
 // doc ready
 $(function () {
+  makeMap(0);
   $('button').click(getTweets);
 });
